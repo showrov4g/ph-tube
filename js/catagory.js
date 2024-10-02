d955de0935f7835f74aaeba5ce32dc0f0a3431cf
showrov4g <showrovg00@gmail.com>
2024-10-02 23:38:09 +0600

    category load using Api

--- js/catagory.js
+++ js/catagory.js
@@ -1,1 +1,21 @@
-console.log("catagory js file");
+// show catagories using API
+
+const loadCategory = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((Response) => Response.json())
+    .then((data) => displayCategory(data.categories))
+    .catch((error) => displayCategory(error));
+};
+// display category function
+
+const displayCategory = (categories) => {
+  const categoryContainer = document.getElementById("categories");
+  categories.forEach((item) => {
+    const button = document.createElement("button");
+    button.classList = "btn";
+    button.innerText = item.category;
+    categoryContainer.append(button);
+  });
+};
+
+loadCategory();
